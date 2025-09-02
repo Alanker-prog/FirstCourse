3b6f81bc8df27f624b4ea1f7fd3f6119b08c6fd2
Alan <yashikalana@mail.ru>
2025-09-02 11:26:45 +0300

    Initial Commit

---
+++ FirstCourse/FirstCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstCourseApp.swift
+//  FirstCourse
+//
+//  Created by Алан Парастаев on 02.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct FirstCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
